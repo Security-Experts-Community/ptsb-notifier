9c70c469d2e57d5648c79d53152c673be6869fff
Kirill Krasnovskiy <kirusha2435896@gmail.com>
2025-01-07 11:29:05 +0300

    update unistall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-docker stop ptsb-notifier
-docker rm ptsb-notifier
-docker rmi builder-ptsb-notifier
+# проверяем, запущен ли контейнер
+if docker ps -q --filter "name=ptsb-notifier" | grep -q .; then
+    echo "Останавливаем контейнер ptsb-notifier..."
+    docker stop ptsb-notifier
+else
+    echo "Контейнер ptsb-notifier не запущен."
+fi
 
-echo "Ptsb-notifier was stopped and deleted from your system"
+echo " "
+
+# проверяем, существует ли контейнер
+if docker ps -a -q --filter "name=ptsb-notifier" | grep -q .; then
+    echo "Удаляем контейнер ptsb-notifier..."
+    docker rm ptsb-notifier
+else
+    echo "Контейнер ptsb-notifier не найден."
+fi
+
+echo " "
+
+# проверяем, существует ли образ
+if docker images --filter "reference=builder-ptsb-notifier" -q | grep -q .; then
+    echo "Удаляем образ builder-ptsb-notifier..."
+    docker rmi builder-ptsb-notifier
+else
+    echo "Образ builder-ptsb-notifier не найден."
+fi
+
+echo " "
+
+echo "Ptsb-notifier был успешно удалён из системы."
